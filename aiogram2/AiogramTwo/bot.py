072857d0a117af78cd886b6676fa989536008f59
Ivan Shalimov <iv.shalimoff@yandex.ru>
2024-09-25 23:04:25 +0300

    New example
    try new example

---
+++ aiogram2/AiogramTwo/bot.py
@@ -0,0 +1,29 @@
+from aiogram import Dispatcher, Bot, html
+import asyncio
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(token='',default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
